fd295ec692c8f1c61d6ca5eefbe0b564847ccd5e
chen_zujun <mustang@api.yunhussm.com>
2017-05-23 15:27:21 +0800

    Iterator使用

--- src/main/java/com/grammar/set/ListTest.java
+++ src/main/java/com/grammar/set/ListTest.java
@@ -9,82 +9,59 @@ import java.util.Set;
 
 public class ListTest {
 
-	public static void testFor(){
-		List<Map<String,Object>> list = new ArrayList<Map<String,Object>>();
-		for (Map<String, Object> map : list) {
-			System.out.println("empty");
-		}
-		
-		List<Map<String,Object>> list2 = null;
-		for (Map<String, Object> map : list2) {
-			System.out.println("null");
-		}
-	}
-	
-	public static void testRemove(){
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(new Integer(1));
-		list.add(new Integer(22));
-		list.add(new Integer(333));
-//		for (Integer in : list) {
-//			if(22 == in){
-//				list.remove(in);
-//			}
-//			System.out.println(in);
-//		}
-//		for (Integer integer : list) {
-////			System.out.println(integer);
-//		}
-		
-		for (Iterator<Integer> iter = list.iterator(); iter.hasNext();) {
-            int in = (Integer) iter.next();
-            if(22 == in){
-            	iter.remove();
-			}
-			System.out.println(in);
+    public static void testFor() {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        for (Map<String, Object> map : list) {
+            System.out.println("empty");
         }
-		
-		for (Integer integer : list) {
-			System.out.println(integer);
-		}
-		
-	}
-	
-	public static void testSetRemove(){
-		Set<Integer> set = new HashSet<Integer>();
-		set.add(new Integer(1));
-		set.add(new Integer(22));
-		set.add(new Integer(333));
 
-//		for (Integer integer : set) {
-//			if(22 == integer){
-//				set.remove(integer);
-//			}
-//		}
-		for (Iterator<Integer> iter = set.iterator(); iter.hasNext();) {
-            int in = (Integer) iter.next();
-            if(22 == in){
-            	iter.remove();
-			}
-			System.out.println(in);
+        List<Map<String, Object>> list2 = null;
+        for (Map<String, Object> map : list2) {
+            System.out.println("null");
         }
-		
-		for (Integer integer : set) {
-			System.out.println(integer);
-		}
-	}
-	
-	public static void main(String[] args) {
-		List<Integer> list = new ArrayList<Integer>();
-		list.add(new Integer(1));
-		list.add(new Integer(22));
-		list.add(new Integer(333));
-		
-		List<Integer> list2 = list.subList(0, 1);
-		list.clear();
-		list=list2;
-		for (Integer integer : list) {
-			System.out.println(integer);
-		}
-	}
+    }
+
+    public static void testListRemove() {
+        List<Integer> list = new ArrayList<Integer>();
+        list.add(new Integer(1));
+        list.add(new Integer(22));
+        list.add(new Integer(333));
+
+        Iterator<Integer> iter = list.iterator();
+        while (iter.hasNext()) {
+            int in = (Integer) iter.next();
+            if (22 == in) {
+                iter.remove();
+            }
+            System.out.println(in);
+        }
+
+        for (Integer integer : list) {
+            System.out.println(integer);
+        }
+
+    }
+
+    public static void testSetRemove() {
+        Set<Integer> set = new HashSet<Integer>();
+        set.add(new Integer(1));
+        set.add(new Integer(22));
+        set.add(new Integer(333));
+
+        Iterator<Integer> iter = set.iterator();
+        while (iter.hasNext()) {
+            int in = (Integer) iter.next();
+            if (22 == in) {
+                iter.remove();
+            }
+        }
+
+        for (Integer integer : set) {
+            System.out.println(integer);
+        }
+    }
+
+    public static void main(String[] args) {
+        testSetRemove();
+    }
 }

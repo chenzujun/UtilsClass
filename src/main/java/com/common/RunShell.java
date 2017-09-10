ad564de5394513d72f00360b1a9361453661e5b6
chen_zujun <mustang@api.yunhussm.com>
2017-09-10 19:41:15 +0800

    格式化

--- src/main/java/com/common/RunShell.java
+++ src/main/java/com/common/RunShell.java
@@ -4,36 +4,35 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 /**
- * 执行脚本文件
- * TODO 完善具体的调用方法；
- *      完善结果获取方法
+ * 执行脚本文件 TODO 完善具体的调用方法； 完善结果获取方法
+ * 
  * @author Administrator
  *
  */
 public class RunShell {
-	public static void main(String[] args) {
-		try {
-			if (System.getProperty("os.name").toUpperCase().contains("WIN")) {
-				// 不做任何处理
-				System.out.println("windows系统！");
-				Process p = Runtime.getRuntime().exec("cmd.exe /c dir");
-				System.out.println(p.toString());
-			} else {
-				String shpath = "/home/demo-echo.sh ";
-				Process ps = Runtime.getRuntime().exec(shpath);
-				ps.waitFor();
+    public static void main(String[] args) {
+        try {
+            if (System.getProperty("os.name").toUpperCase().contains("WIN")) {
+                // 不做任何处理
+                System.out.println("windows系统！");
+                Process p = Runtime.getRuntime().exec("cmd.exe /c dir");
+                System.out.println(p.toString());
+            } else {
+                String shpath = "/home/demo-echo.sh ";
+                Process ps = Runtime.getRuntime().exec(shpath);
+                ps.waitFor();
 
-				BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
-				StringBuffer sb = new StringBuffer();
-				String line;
-				while ((line = br.readLine()) != null) {
-					sb.append(line).append("\n");
-				}
-				String result = sb.toString();
-				System.out.println(result);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+                BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+                StringBuffer sb = new StringBuffer();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line).append("\n");
+                }
+                String result = sb.toString();
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }

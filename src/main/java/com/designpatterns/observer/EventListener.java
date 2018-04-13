06010d97374ffb5bbbd45dd41769c05961721c27
chen_zujun <chen_zujun@163.com>
2018-04-13 23:36:32 +0800

    使用guaua EventListener实现观察者模式

---
+++ src/main/java/com/designpatterns/observer/EventListener.java
@@ -0,0 +1,23 @@
+package com.designpatterns.observer;
+
+import com.google.common.eventbus.Subscribe;
+
+/**
+ * 消息接受类
+ * 
+ * @author chen_
+ *
+ */
+public class EventListener {
+	public int lastMessage = 0;
+
+	@Subscribe
+	public void listen(TestEvent event) {
+		lastMessage = event.getMessage();
+		System.out.println("Message:" + lastMessage);
+	}
+
+	public int getLastMessage() {
+		return lastMessage;
+	}
+}

5795b31ba87bb23e683dc1958f64d817360c403e
chen_zujun <chen_zujun@163.com>
2018-04-24 00:09:54 +0800

    for删除问题测试

--- src/main/java/com/grammar/collection/list/ArrayListTest.java
+++ src/main/java/com/grammar/collection/list/ArrayListTest.java
@@ -8,21 +8,42 @@ public class ArrayListTest {
 
     public static void testListRemove() {
         List<Integer> list = new ArrayList<Integer>();
-        list.add(new Integer(1));
-        list.add(new Integer(22));
-        list.add(new Integer(333));
+        list.add(0);
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+        list.add(7);
 
-        Iterator<Integer> iter = list.iterator();
-        while (iter.hasNext()) {
-            int in = (Integer) iter.next();
-            if (22 == in) {
-                iter.remove();
+        //正常循环
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("i的值:" + i + " 对应的数字:" + list.get(i));
+        }
+        System.out.println("没有remove前list的项:" + list.size());
+
+        //边循环边删除
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("i的值:" + i + " 对应的数字:" + list.get(i));
+            //删除list的第四项
+            if (list.get(i) == 3) {
+                list.remove(list.get(i));
             }
-            System.out.println(in);
+        }
+        System.out.println("remove后list的项:" + list.size());
+
+        Iterator<Integer> it = list.iterator();
+        while(it.hasNext()){
+            Integer v = it.next();
+            if(3 == v){
+                it.remove();
+            }
         }
 
-        for (Integer integer : list) {
-            System.out.println(integer);
+        System.out.println("==========remove后的list==========");
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println("i的值:" + i + " 对应的数字:" + list.get(i));
         }
 
     }

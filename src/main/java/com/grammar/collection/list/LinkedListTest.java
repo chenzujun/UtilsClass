0142ae656a213407f9dd25b9c6bd262d6d30dc7a
chen_zujun <chen_zujun@163.com>
2018-06-05 21:51:19 +0800

    JAVA8学习

---
+++ src/main/java/com/grammar/collection/list/LinkedListTest.java
@@ -0,0 +1,40 @@
+package com.grammar.collection.list;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @description:
+ * @author: mustang
+ * @create: 2018-04-25
+ **/
+public class LinkedListTest {
+    public static void main(String[] args) {
+        Queue<String> queue = new LinkedList<>();
+        //添加元素
+        queue.offer("a");
+        queue.offer("b");
+        queue.offer("c");
+        queue.offer("d");
+        queue.offer("e");
+        for(String q : queue){
+            System.out.println(q);
+        }
+        System.out.println("===");
+        System.out.println("poll="+queue.poll()); //返回第一个元素，并在队列中删除
+        for(String q : queue){
+            System.out.println(q);
+        }
+        System.out.println("===");
+        System.out.println("element="+queue.element()); //返回第一个元素
+        for(String q : queue){
+            System.out.println(q);
+        }
+        System.out.println("===");
+        System.out.println("peek="+queue.peek()); //返回第一个元素
+        for(String q : queue){
+            System.out.println(q);
+        }
+    }
+}

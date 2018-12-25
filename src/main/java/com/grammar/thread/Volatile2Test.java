8e13afc307736e63a9dd4fe79ec88535b843de6c
chen_zujun@163 <github1121>
2018-12-25 15:57:43 +0800

    volatile测试

--- src/main/java/com/grammar/thread/Volatile2Test.java
+++ src/main/java/com/grammar/thread/Volatile2Test.java
@@ -7,10 +7,18 @@ package com.grammar.thread;
  **/
 public class Volatile2Test {
     static int i = 0, j = 0;
-    static synchronized void one() { i++; j++; }
+    static synchronized void one() {
+        i++;
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        j++;
+    }
     static synchronized void two() {
         if(i != j){
-            System.out.println("i="+i + " j=" + j);
+            System.out.println("i="+i + " j=" + j +"two start time="+System.currentTimeMillis());
         }
     }
     public static void main(String[] args) {

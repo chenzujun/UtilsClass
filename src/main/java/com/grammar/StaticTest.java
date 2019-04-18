dde13d27aee6acd6f62ca147cc3209a822386333
chen_zujun@163 <github1121>
2019-04-18 10:46:12 +0800

    desc: final编译测试 reviewer：陈军

--- src/main/java/com/grammar/StaticTest.java
+++ src/main/java/com/grammar/StaticTest.java
@@ -2,6 +2,11 @@ package com.grammar;
 
 public class StaticTest
 {
+
+    public static final String HK_ISSUE_PUTAWAY_MODEL = "您的包裹已退回倉庫，請在辦工時間致電：852-30183617，儘快處理避免產生倉租，謝謝";
+    static String hk4PLmodel = "包裹{0}抵達{1},提貨碼{2}免租期{3}天，超期10HKD/天。請儘快取貨";
+    public final String HK_ISSUE_PUTAWAY_MODELS = "您的包裹已退回倉庫，請在辦工時間致電：852-30183617，儘快處理避免產生倉租，謝謝";
+
     public static void main(String[] args)
     {
         staticFunction();
@@ -30,6 +35,10 @@ public class StaticTest
     }
  
     public static void staticFunction(){
+        System.out.println(HK_ISSUE_PUTAWAY_MODEL);
+        System.out.println(hk4PLmodel);
+        StaticTest s = new StaticTest();
+        System.out.println(s.HK_ISSUE_PUTAWAY_MODELS);
         System.out.println(b);
     }
  

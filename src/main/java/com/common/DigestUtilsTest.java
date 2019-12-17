049c5e6ee209bf0894ec708722033f2dec9c88e0
chen_zujun@163 <github1121>
2019-12-17 10:30:05 +0800

    desc: md5加密 reviewer：chenjun

---
+++ src/main/java/com/common/DigestUtilsTest.java
@@ -0,0 +1,41 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.common;
+
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+public class DigestUtilsTest {
+
+    /**
+     * 生成sign：业务数据+私钥 用md5加密
+     *
+     * @param data
+     * @return
+     */
+    public static String getSign(String data){
+        // 对应appkey=56A496586265422397F72A9991A3F4A9
+        String secretKey = "DBCD8821C9094514B653E005116CEF45";
+        return DigestUtils.md5Hex(data+secretKey);
+    }
+
+    public static void main(String[] args) {
+        String bizdata = "{\"verificationCode\":\"121701\",\"appOperationTime\":1573783087854,\"userId\":\"68c837d65b294252a66707b5fccd227c\",\"userName\":\"S9614\",\"pudoName\":\"4PX-火炭坳背灣 HK003點\",\"pudoCode\":\"HKG-P005\",\"siteType\":\"TAKE_PUDO\"}";
+        System.out.println(getSign(bizdata));
+    }
+}

945060ab72214af72d13044d91c6a4b895ebfe9d
chen_zujun@163 <github1121>
2020-03-12 11:00:58 +0800

    desc: http请求工具类优化 reviewer：chenjun

--- src/main/java/com/common/util/HttpUtils.java
+++ src/main/java/com/common/util/HttpUtils.java
@@ -84,18 +84,17 @@ public class HttpUtils {
             if (response.getStatusLine().getStatusCode() == 200) {
                 return EntityUtils.toString(response.getEntity());
             } else {
-                logger.error("访问路径：" + url);
+                logger.error("访问路径：{} | statuscode：{}" , url, response.getStatusLine().getStatusCode());
             }
-
         } catch (IOException e) {
-            logger.error("调用失败：" + e.getMessage(), e);
+            logger.error("请求异常：{}", e);
         } finally {
-            try {
-                if (response != null) {
+            if (response != null) {
+                try {
                     response.close();
+                } catch (IOException e) {
+                    logger.error("response.close exception：{}", e);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
             }
         }
         return null;
@@ -110,43 +109,7 @@ public class HttpUtils {
      * @return
      */
     public static String post(String url, Map<String, String> params)  {
-        HttpPost httpPost = new HttpPost(url);
-        if (params != null) {
-            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
-            Set<String> keySet = params.keySet();
-            for (String key : keySet) {
-                nvps.add(new BasicNameValuePair(key, params.get(key)));
-            }
-
-            try {
-                httpPost.setEntity(new UrlEncodedFormEntity(nvps, "UTF-8"));
-            } catch (UnsupportedEncodingException e1) {
-                e1.printStackTrace();
-            }
-        }
-
-        CloseableHttpResponse response = null;
-        try {
-            response = (CloseableHttpResponse) httpClient.execute(httpPost);
-            if (response.getStatusLine().getStatusCode() == 200) {
-                HttpEntity httpEntity = response.getEntity();
-                return EntityUtils.toString(httpEntity);
-            } else {
-                logger.error("访问路径：" + url);
-            }
-
-        } catch (IOException e) {
-            logger.error("调用失败：" + e.getMessage(), e);
-        } finally {
-            try {
-                if (response != null) {
-                    response.close();
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        return null;
+        return post(url, params, null);
     }
 
     /**
@@ -160,7 +123,7 @@ public class HttpUtils {
     public static String post(String url, Map<String, String> params, String encode)  {
         HttpPost httpPost = new HttpPost(url);
         if (params != null) {
-            List<NameValuePair> nvps = new ArrayList<NameValuePair>();
+            List<NameValuePair> nvps = new ArrayList<>();
             Set<String> keySet = params.keySet();
             for (String key : keySet) {
                 nvps.add(new BasicNameValuePair(key, params.get(key)));
@@ -184,18 +147,61 @@ public class HttpUtils {
                     return new String(EntityUtils.toString(httpEntity).getBytes("ISO-8859-1"), encode);
                 }
             } else {
-                logger.info("访问路径：" + url);
+                logger.error("访问路径：{} | statuscode：{}" , url, response.getStatusLine().getStatusCode());
             }
-
         } catch (IOException e) {
-            logger.error("调用失败：" + e.getMessage(), e);
+            logger.error("请求异常：{}", e);
         } finally {
-            try {
-                if (response != null) {
+            if (response != null) {
+                try {
                     response.close();
+                } catch (IOException e) {
+                    logger.error("response.close exception：{}", e);
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    public static String postJson(String url, Object params) {
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "application/json");
+        if (params != null) {
+            Gson gson = new Gson();
+            String paramStr = gson.toJson(params);
+            httpPost.setEntity(new StringEntity(paramStr, Charset.forName("UTF-8")));
+        }
+
+        return postExecute(url, httpPost);
+    }
+
+    public static String postTextplain(String url, String params) {
+        HttpPost httpPost = new HttpPost(url);
+        httpPost.addHeader("Content-Type", "text/plain");
+        if (params != null) {
+            httpPost.setEntity(new StringEntity(params, Charset.forName("UTF-8")));
+        }
+        return postExecute(url, httpPost);
+    }
+
+    private static String postExecute(String url, HttpPost httpPost) {
+        CloseableHttpResponse response = null;
+        try {
+            response = (CloseableHttpResponse)httpClient.execute(httpPost);
+            if (response.getStatusLine().getStatusCode() == 200) {
+                return EntityUtils.toString(response.getEntity());
+            } else {
+                logger.error("访问路径：{} | statuscode：{}" , url, response.getStatusLine().getStatusCode());
+            }
+        } catch (IOException e) {
+            logger.error("请求异常：{}", e);
+        } finally {
+            if (response != null) {
+                try {
+                    response.close();
+                } catch (Throwable e) {
+                    logger.error("response.close exception：{}", e);
+                }
             }
         }
         return null;
@@ -210,8 +216,7 @@ public class HttpUtils {
      */
     private static HttpRequestBase setHeaders(HttpRequestBase request, Map<String, String> headers) {
         Iterator<String> keyIterator = headers.keySet().iterator();
-        String key = null;
-
+        String key;
         while (keyIterator.hasNext()) {
             key = keyIterator.next();
             request.addHeader(key, headers.get(key));
@@ -220,84 +225,4 @@ public class HttpUtils {
         return request;
     }
 
-    public static String postJson(String url, Object params) {
-        HttpPost httpPost = new HttpPost(url);
-        httpPost.addHeader("Content-Type", "application/json");
-        if (params != null) {
-            Gson gson = new Gson();
-            String paramStr = gson.toJson(params);
-            httpPost.setEntity(new StringEntity(paramStr, Charset.forName("UTF-8")));
-        }
-
-        try {
-            CloseableHttpResponse response = (CloseableHttpResponse)httpClient.execute(httpPost);
-            Throwable var4 = null;
-            String var5 = null;
-            try {
-                if (response.getStatusLine().getStatusCode() != 200) {
-                    logger.error("请求失败URL:{} , StatusCode:{}", new Object[]{url, response.getStatusLine().getStatusCode()});
-                }
-
-                var5 = EntityUtils.toString(response.getEntity());
-            } catch (Throwable var15) {
-                var4 = var15;
-            } finally {
-                if (response != null) {
-                    if (var4 != null) {
-                        try {
-                            response.close();
-                        } catch (Throwable var14) {
-                            var4.addSuppressed(var14);
-                        }
-                    } else {
-                        response.close();
-                    }
-                }
-
-            }
-            return var5;
-        } catch (Exception var17) {
-            logger.error("请求异常：" + var17.getMessage(), var17);
-            return null;
-        }
-    }
-
-    public static String postTextplain(String url, String params) {
-        HttpPost httpPost = new HttpPost(url);
-        httpPost.addHeader("Content-Type", "text/plain");
-        if (params != null) {
-            httpPost.setEntity(new StringEntity(params, Charset.forName("UTF-8")));
-        }
-        try {
-            CloseableHttpResponse response = (CloseableHttpResponse)httpClient.execute(httpPost);
-            Throwable var4 = null;
-            String var5 = null;
-            try {
-                if (response.getStatusLine().getStatusCode() != 200) {
-                    logger.error("请求失败URL:{} , StatusCode:{}", new Object[]{url, response.getStatusLine().getStatusCode()});
-                }
-
-                var5 = EntityUtils.toString(response.getEntity());
-            } catch (Throwable var15) {
-                var4 = var15;
-            } finally {
-                if (response != null) {
-                    if (var4 != null) {
-                        try {
-                            response.close();
-                        } catch (Throwable var14) {
-                            var4.addSuppressed(var14);
-                        }
-                    } else {
-                        response.close();
-                    }
-                }
-
-            }
-            return var5;
-        } catch (Exception var17) {
-            logger.error("请求异常：" + var17.getMessage(), var17);
-            return null;
-        }
-    }
 }

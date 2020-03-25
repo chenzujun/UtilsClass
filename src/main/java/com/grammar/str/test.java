5e9614db7718688d0f50b90e84acbe4f4765dc13
chen_zujun@163 <github1121>
2020-03-25 16:05:23 +0800

    desc: 精简 reviewer：chenjun

--- src/main/java/com/grammar/str/test.java
+++
@@ -1,9 +0,0 @@
-package com.grammar.str;
-
-public class test {
-	public static void main(String[] args) {
-		String str = "45645";
-		System.out.println(str.substring(0, str.length()-2));
-		System.out.println(str);
-	}
-}

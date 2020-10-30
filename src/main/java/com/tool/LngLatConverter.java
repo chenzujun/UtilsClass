310ea673314021c9dde10ca8c5d379146006604a
chenjun <chenjun01@4px.com>
2020-10-30 17:34:29 +0800

    经纬度批量转换方法完善

--- src/main/java/com/tool/LngLatConverter.java
+++ src/main/java/com/tool/LngLatConverter.java
@@ -16,11 +16,28 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-
+/**
+ * 坐标转换
+ * @author chenj
+ */
 public class LngLatConverter {
 
 
     private static double x_pi = 3.14159265358979324 * 3000.0 / 180.0;
+
+    /**
+     * 将火星坐标转变成百度坐标
+     *
+     * @param lngLat_gd 火星坐标（高德、腾讯地图坐标等）
+     * @return 百度坐标
+     */
+
+    public static LngLat bd_encrypt(LngLat lngLat_gd) {
+        double x = lngLat_gd.getLng(), y = lngLat_gd.getLat();
+        double z = Math.sqrt(x * x + y * y) + 0.00002 * Math.sin(y * x_pi);
+        double theta = Math.atan2(y, x) + 0.000003 * Math.cos(x * x_pi);
+        return new LngLat(dataDigit(6, z * Math.cos(theta) + 0.0065), dataDigit(6, z * Math.sin(theta) + 0.006));
+    }
 
     /**
      * 将百度坐标转变成火星坐标
@@ -41,7 +58,7 @@ public class LngLatConverter {
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\chenjun01\\Desktop\\orgregion1.txt")));
+        BufferedReader br = new BufferedReader(new FileReader(new File("C:\\Users\\chenjun01\\Desktop\\orgregion.txt")));
         String s;
         int i = 0;
         while ((s = br.readLine()) != null) {
